c02bee0dce1e238ff30bb35a31fc02461ac0de13
Daniel Lorand <daniel.lorand97@gmail.com>
2021-03-22 12:05:27 +0200

    update

--- Api/src/Controller/ApiController.php
+++ Api/src/Controller/ApiController.php
@@ -11,65 +11,13 @@ class ApiController extends ControllerBase {
 
      public function index() {
     return new JsonResponse([ 'data' => $this->getResults(), 'method' => 'GET', 'status'=> 200]);
-    //return $this->getResults();
   }
 
-
-  public function checkTerm($term){
-    if($term != null){
-      $newterm = $term;
-      $query = \Drupal::entityQuery('taxonomy_term');
-      $query->condition('vid', "movies");
-      $query->condition('name',  $newterm);
-      $tids = $query->execute();
-      if(!$tids){
-        $term = \Drupal\taxonomy\Entity\Term::create([
-            'vid' => 'movies',
-            'name' =>  $newterm,
-            ]);
-        $term->save();
-         return 1 ;
-      }
-    }else{
-     return 0;
-    }
-  }
- 
-
-  public function getTermID($term){
-    if($term != null){
-       $term = \Drupal::entityTypeManager()->getStorage('taxonomy_term')
-                                  ->loadByProperties(['name' => $term, 'vid' => 'movies']);
-                                $term = reset($term);
-                                $term_id = $term->id();
-                                return $term_id;
-    }else{
-       $term = \Drupal::entityTypeManager()->getStorage('taxonomy_term')
-        ->loadByProperties(['name' => 'unknown', 'vid' => 'movies']);
-          $term = reset($term);
-         $term_id = $term->id();
-          return $term_id;
-    }
-
-  }
-
-  public function deleteTerms(){
-     $tids = \Drupal::entityQuery('taxonomy_term')
-    ->condition('vid', 'movies')
-    ->execute();
-
-  $controller = \Drupal::entityTypeManager()->getStorage('taxonomy_term');
-  $entities = $controller->loadMultiple($tids);
-  $controller->delete($entities);
-  }
   public function getResults(){
 
     $json = file_get_contents("https://eventbook.ro/api/events/?festival_id=73");
    
     $obj = json_decode($json);;
-
-
-   // $this->deleteTerms();
 
   foreach($obj as $obj1 ){
     $this->checkTerm($obj1->section);
@@ -100,16 +48,12 @@ class ApiController extends ControllerBase {
                                     'target_id' => $file->id(),
                                     'title' => $file->getFilename(),
                                     'filename' => $file->getFilename(),
-                                  
                                   ),
                                 ),
                                'field_movies_s' =>  $this->getTermID($obj1->section),
                                'field_movies_year' =>  $this->getTermID($obj1->year),
-                                ]);
-                           
-                                
-                            $entity->save();
-
+                                ]); 
+  $entity->save();
   } 
         
   $result[]= [
@@ -122,33 +66,43 @@ class ApiController extends ControllerBase {
         'cast' => $obj1->cast, 
     ];
   }
-   
-  return $result;
-  //return new JsonResponse([ 'data' => $result, 'method' => 'GET', 'status'=> 200]);
+  return new JsonResponse([ 'data' => $result]);
 }
 
-    public function getData() {
-    $query = \Drupal::entityQuery('node')
-      ->condition('type', 'movies')
-      ->sort('title', 'DESC');
-    $nodes_ids = $query->execute();
-    if ($nodes_ids) {
-      foreach ($nodes_ids as $node_id) {
-        $node = \Drupal\node\Entity\Node::load($node_id);
-        $result[] = [
-          "id" => $node->id(),
-          "title" => $node->getTitle(),
-          "body" => $node->get('body')->getValue(),
-          "duration" => $node->get('field_movie_')->getValue(),
-          "cast" => $node->get('field_movies_cast')->getValue(),
-          "image" => $node->get('field_movies_image')->getValue(),
-          "section" => $node->get('field_movies_s')->getValue(),
-          "year" => $node->get('field_movies_year')->getValue(),
-     
-        ];
+public function checkTerm($term){
+    if($term != null){
+      $newterm = $term;
+      $query = \Drupal::entityQuery('taxonomy_term');
+      $query->condition('vid', "movies");
+      $query->condition('name',  $newterm);
+      $tids = $query->execute();
+      if(!$tids){
+        $term = \Drupal\taxonomy\Entity\Term::create([
+            'vid' => 'movies',
+            'name' =>  $newterm,
+            ]);
+        $term->save();
+         return 1 ;
       }
+    }else{
+     return 0;
     }
-    return $result;
   }
+  public function getTermID($term){
+    if($term != null){
+       $term = \Drupal::entityTypeManager()->getStorage('taxonomy_term')
+                                  ->loadByProperties(['name' => $term, 'vid' => 'movies']);
+                                $term = reset($term);
+                                $term_id = $term->id();
+                                return $term_id;
+    }else{
+       $term = \Drupal::entityTypeManager()->getStorage('taxonomy_term')
+        ->loadByProperties(['name' => 'unknown', 'vid' => 'movies']);
+          $term = reset($term);
+         $term_id = $term->id();
+          return $term_id;
+    }
+  }
+
 
 }
